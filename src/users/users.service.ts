64412776a64e7f99d32c799db399b176b92990d3
techielukesh <alltechfactzzz@gmail.com>
2026-07-11 16:34:13 +0530

    created repo+basic CRUD equivalent to SQL

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  findAll() {
+    return this.usersRepository.find();
+  }
+
+  async findByEmail(email: string) {
+    return this.usersRepository.findOne({
+      where: {
+        email,
+      },
+    });
+  }
+
+  async create(user: Partial<User>) {
+    const newUser = this.usersRepository.create(user);
+
+    return this.usersRepository.save(newUser);
+  }
+}
